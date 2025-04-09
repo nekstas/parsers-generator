2cd4d8ced16c30b0bf1d73e57717985eca064496
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-04-09 18:44:58 +0300

    Add grammar parser data files

---
+++ core/grammar_parser/data/identifier.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <cstdint>
+
+namespace pg {
+enum class Identifier : size_t { Grammar, Production, ProductionList, Rule, RuleHeader, RuleList, SequenceOfSymbols, Symbol, TakeSymbol, };
+}
---
+++ core/grammar_parser/data/token_type.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <cstdint>
+
+namespace pg {
+enum class TokenType : size_t { ArrowRight, At, CloseSq, CloseTr, Colon, Dollar, Eof, Name, OpenSq, OpenTr, Skip, Unknown, };
+}
